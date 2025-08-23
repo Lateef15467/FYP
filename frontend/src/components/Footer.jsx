f8f18c430865d961be028caf9127e55f420275fe
lateef <lateef4262@gmail.com>
2025-08-23 09:40:37 +0500

    home pics

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-m">
+      <div className="flex flex-col sm:grid grid-cols-[2fr_1fr_1fr] gap-14 my-10 mt-40 text-m">
         <div className="mb-5 w-32">
           <Link to="/">
             <h1 className="text-4xl mb-8">
@@ -14,9 +14,9 @@ const Footer = () => {
               </span>
             </h1>
           </Link>
-          <p className="w-full md:w-2/3 text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-            libero quae minus4 maxime
+          <p className="w-full md:w-40 text-gray-600">
+            This is a fully functional an e-commerce website application which
+            is built in mern stack.
           </p>
         </div>
         <div>
@@ -25,11 +25,15 @@ const Footer = () => {
             <Link to={"/"}>
               <li>Home</li>
             </Link>
+            <Link to={"/collection"}>
+              <li>Collection</li>
+            </Link>
+            <Link to={"/contact"}>
+              <li>Contact</li>
+            </Link>
             <Link to={"/about"}>
               <li>About</li>
             </Link>
-            <li>Delivery</li>
-            <li>Privacy Policy</li>
           </ul>
         </div>
         <div className=" ">
@@ -37,6 +41,8 @@ const Footer = () => {
           <ul className="flex flex-col gap-1 text-gray-600">
             <li>+1 234 234 23423</li>
             <li>lateef@gmail.com </li>
+            <li>+92 334 234 234</li>
+            <li>admin@gmail.com </li>
           </ul>
         </div>
       </div>
--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -14,7 +14,7 @@ const LatestCollection = () => {
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
-        <Title text1={"Latest"} text2={"Collections"}></Title>
+        <Title text1={"Latest"} text2={" Collections"}></Title>
         <p className="w-3/4 m-auto text-xs sm:text-sm:text-sm md:text-base text-gray-600">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore eum
           ut, non facere laboriosam asperiores assumenda
