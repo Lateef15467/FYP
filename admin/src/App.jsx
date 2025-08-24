44d0a1dd4ceb2d616d4231ee1a776b74e6b5fe7f
lateef <lateef4262@gmail.com>
2025-08-24 19:17:56 +0500

    25 aug

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import SideBar from "./components/SideBar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import Order from "./pages/Order";
+import List from "./pages/List";
 
 const App = () => {
   return (
@@ -10,6 +14,13 @@ const App = () => {
         <hr />
         <div className="flex w-full">
           <SideBar></SideBar>
+          <div className="w-[70%] mx-auto ml-[max{5vw,25px}] my-8 text-gray-600 text-base">
+            <Routes>
+              <Route path="/add" element={<Add></Add>}></Route>
+              <Route path="/list" element={<List></List>}></Route>
+              <Route path="/list" element={<Order></Order>}></Route>
+            </Routes>
+          </div>
         </div>
       </>
     </div>
--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -15,14 +15,14 @@ const SideBar = () => {
         </NavLink>
         <NavLink
           className="flex items-center gap-3 border-gray-300 border-r-0 px-3 py-2 rounded-1"
-          to="/add"
+          to="/list"
         >
           <img className="w-5 h-5" src={assets.order_icon} />
           <p className="hidden md:block">List items</p>
         </NavLink>
         <NavLink
           className="flex items-center gap-3 border-gray-300 border-r-0 px-3 py-2 rounded-1"
-          to="/add"
+          to="/orders"
         >
           <img className="w-5 h-5" src={assets.order_icon} />
           <p className="hidden md:block">Orders </p>
