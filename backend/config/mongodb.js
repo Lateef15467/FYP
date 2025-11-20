82719aafe10ffefd4c7744a946f218b3e8774490
lateef <lateef4262@gmail.com>
2025-11-20 12:54:59 +0500

    update mongodb config

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,9 +4,13 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("DB connected");
   });
-  // for production
-  await mongoose.connect(`${process.env.MONGODB_URI}/MERN`);
-  // for local development
-  // await mongoose.connect(process.env.MONGODB_URI);
+
+  // Use production DB by default
+  // await mongoose.connect(`${process.env.MONGODB_URI}/MERN`);
+
+  // If you want to use local DB during development,
+
+  await mongoose.connect(process.env.MONGODB_URI);
 };
+
 export default connectDB;
